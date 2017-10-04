a5fb0956c649a06c3aee51181336bc1ecf6f542e
Renqian Luo <lrqrichard@gmail.com>
2017-10-04 15:26:58 +0800

    v1.2.4

--- DLT2T/data_generators/all_problems.py
+++ DLT2T/data_generators/all_problems.py
@@ -35,6 +35,7 @@ from DLT2T.data_generators import snli
 from DLT2T.data_generators import wiki
 from DLT2T.data_generators import wmt
 from DLT2T.data_generators import wsj_parsing
+from DLT2T.data_generators import dual_learning
 
 
 # Problem modules that require optional dependencies
