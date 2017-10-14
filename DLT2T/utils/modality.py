61fd7e3456bfbc2b98837c873e3a7e987380c654
Renqian Luo <lrqrichard@gmail.com>
2017-10-14 16:31:35 +0800

    debug loss

--- DLT2T/utils/modality.py
+++ DLT2T/utils/modality.py
@@ -171,7 +171,7 @@ class Modality(object):
                                                    tf.add_n(sharded_scalar_loss_den))
       loss_num = tf.concat(tf.reduce_sum(sharded_loss_num, [1,2,3]), 0)
       loss_den = tf.concat(tf.reduce_sum(sharded_loss_den, [1,2,3]), 0)
-      print(sharded_loss_num.get_shape(), sharded_loss_den.get_shape())
+      print(loss_num.get_shape(), loss_den.get_shape())
       loss_num = tf.Print(loss_num, [loss_num])
       loss_den = tf.Print(loss_den, [loss_den])
       loss = loss_num / tf.maximum(tf.ones(tf.shape(loss_den), loss_den))
