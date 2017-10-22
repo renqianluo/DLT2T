bc6508c3f0e71d5906176d033d9049ac1b39c66c
Renqian Luo <lrqrichard@gmail.com>
2017-10-23 03:24:26 +0800

    debug

--- DLT2T/utils/model_builder.py
+++ DLT2T/utils/model_builder.py
@@ -138,6 +138,8 @@ def model_fn(model,
       else:
         with tf.variable_scope("B2A"):
           features["input_space_id"], features["target_space_id"] = features["target_space_id"], features["input_space_id"]
+          features["input_space_id"] = [problem.SpaceID.EN_TOK]
+          features["target_space_id"] = [problem.SpaceID.DE_TOK]
           features["input_space_id"] = tf.Print(features["input_space_id"], [features["input_space_id"]])
           features["target_space_id"] = tf.Print(features["target_space_id"], [features["target_space_id"]])
           return model_class.infer(
